53b9705dc7a118d009f101cd16d4f8ddcbc0ddc5
STEVEN JOYCE - STUDENT <G00362012@gmit.ie>
2019-09-25 10:34:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Content from './components/content';
+import Footer from './components/footer';
+import Header from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import{ Switch, Route, BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  //To turn function to class add class name and extend app to include React.Component
+  //Put the return in a render
+  render(){
+    return (
+      <BrowserRouter> 
+        <div className="App">
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+            
+          </Navbar>
+            <Switch>
+              <Route exact path="/" component={Header}/>
+              <Route path="/create" component={Footer}/>
+              <Route path="/read" component={Content}/>
+            </Switch>
+        </div>
+    </BrowserRouter>
+    );
+  }
+  
 }
 
 export default App;
